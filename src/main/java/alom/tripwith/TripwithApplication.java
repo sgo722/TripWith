e6f85e235191e7d5da185c08c3515f52db737f24
sgo722 <sgo722@naver.com>
2023-11-09 18:47:14 +0900

    inital project

---
+++ src/main/java/alom/tripwith/TripwithApplication.java
@@ -0,0 +1,13 @@
+package alom.tripwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripwithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alom/tripwith/TripwithApplicationTests.java
@@ -0,0 +1,13 @@
+package alom.tripwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
